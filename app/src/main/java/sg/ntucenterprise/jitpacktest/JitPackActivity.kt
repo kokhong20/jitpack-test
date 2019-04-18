6fc1a513e58ee62aadfb6864b7e7e611c399ef23
Alvin Choo <alvin_choo@ntucenterprise.sg>
2019-04-18 14:53:27 +0800

    Remove library

--- app/src/main/java/sg/ntucenterprise/jitpacktest/JitPackActivity.kt
+++ app/src/main/java/sg/ntucenterprise/jitpacktest/JitPackActivity.kt
@@ -3,7 +3,7 @@ package sg.ntucenterprise.jitpacktest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class JitPackActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
